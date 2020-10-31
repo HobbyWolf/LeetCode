47248b1a0bc3ef2a783ba3b7e346792486cbd25c
Sachin Menon <sachunira@gmail.com>
2020-10-31 11:00:39 +0100

    Final solution with optimized sol for reference

--- nQueens.cpp
+++ nQueens.cpp
@@ -29,8 +29,8 @@ vector<vector<string>> store_queen(vector<vector<int>> master_q, int n)
 		board.reserve(n);
 		for(auto row:comb)
 		{
-			string q('.', n);
-			q[row] = 'Q';
+			string q(n, '.');
+			q[row-1] = 'Q';
 			board.push_back(q);
 		}
 		queens.push_back(board);
@@ -56,12 +56,6 @@ vector<vector<string>> solveNQueens(int n) {
 		add_cell.push_back(j+1);
 		col_temp.remove(j);
 		place_queens(2, n, col_temp, sub_cell, add_cell, queen_pos);
-	}
-	for(auto k: master)
-	{
-		for(auto j: k)
-			std::cout<<j<<"->";
-		std::cout<<std::endl;
 	}
 	return store_queen(master, n);
 }
@@ -98,3 +92,34 @@ int main()
 {
 	solveNQueens(5);
 }
+
+
+// Optimized solution
+
+/*
+class Solution {
+public:
+    std::vector<std::vector<std::string> > solveNQueens(int n) {
+        std::vector<std::vector<std::string> > res;
+        std::vector<std::string> nQueens(n, std::string(n, '.'));
+        std::vector<int> flag_col(n, 1), flag_45(2 * n - 1, 1), flag_135(2 * n - 1, 1);
+        solveNQueens(res, nQueens, flag_col, flag_45, flag_135, 0, n);
+        return res;
+    }
+private:
+    void solveNQueens(std::vector<std::vector<std::string> > &res, std::vector<std::string> &nQueens, std::vector<int> &flag_col, std::vector<int> &flag_45, std::vector<int> &flag_135, int row, int &n) {
+        if (row == n) {
+            res.push_back(nQueens);
+            return;
+        }
+        for (int col = 0; col != n; ++col)
+            if (flag_col[col] && flag_45[row + col] && flag_135[n - 1 + col - row]) {
+                flag_col[col] = flag_45[row + col] = flag_135[n - 1 + col - row] = 0;
+                nQueens[row][col] = 'Q';
+                solveNQueens(res, nQueens, flag_col, flag_45, flag_135, row + 1, n);
+                nQueens[row][col] = '.';
+                flag_col[col] = flag_45[row + col] = flag_135[n - 1 + col - row] = 1;
+            }
+    }
+};
+*/
