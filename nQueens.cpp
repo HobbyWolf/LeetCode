19996a147bceda7874d9d1da8e2a100065bfa64f
Sachin Menon <sachunira@gmail.com>
2020-10-31 10:16:42 +0100

    Added local storage

--- nQueens.cpp
+++ nQueens.cpp
@@ -17,7 +17,7 @@ using std::find;
 
 vector<vector<vector<int>>> master;
 
-void place_queens(int, int, list<int>, list<int>, list<int>);
+void place_queens(int, int, list<int>, list<int>, list<int>, vector<int>);
 
 void solveNQueens(int n) {
 
@@ -27,22 +27,29 @@ void solveNQueens(int n) {
 		col.push_back(i);
 	for(int j = 1; j<n/2.0+1; j++)
 	{
+		vector<int> queen_pos;
+		queen_pos.reserve(n);
+		queen_pos.emplace_back(j);
 		col_temp = col;
 		sub_cell.push_back(j-1);
 		add_cell.push_back(j+1);
 		col_temp.remove(j);
 		std::cout<<"1 - >"<<j<<std::endl;
-		place_queens(2, n, col_temp, sub_cell, add_cell);
+		place_queens(2, n, col_temp, sub_cell, add_cell, queen_pos);
 	}
 }
 
-void place_queens(int start, int nq, list<int> colq, list<int> sub_cellq, list<int> add_cellq)
+void place_queens(int start, int nq, list<int> colq, list<int> sub_cellq, list<int> add_cellq, vector<int> queen_posq)
 {
-
+	if(start>nq)
+	{
+		
+	}
 	while(start <= nq)
 	{
 		for(auto a:colq)
 		{
+			auto queen_posq_temp = queen_posq;
 			int start_temp = start;
 			auto colq_temp = colq;
 			auto sub_cellq_temp = sub_cellq;
@@ -51,11 +58,12 @@ void place_queens(int start, int nq, list<int> colq, list<int> sub_cellq, list<i
 			auto it_add = find(add_cellq.begin(), add_cellq.end(), a+start);
 			if(it_sub == sub_cellq.end() && it_add == add_cellq.end())
 			{
+				queen_posq_temp.push_back(a);
 				std::cout<<start<<"- >"<<a<<std::endl;
 				sub_cellq_temp.push_back(a-start);
 				add_cellq_temp.push_back(a+start);
 				colq_temp.remove(a);
-				place_queens(++start_temp, nq, colq_temp, sub_cellq_temp, add_cellq_temp);
+				place_queens(++start_temp, nq, colq_temp, sub_cellq_temp, add_cellq_temp, queen_posq_temp);
 			}
 		}
 		break;
